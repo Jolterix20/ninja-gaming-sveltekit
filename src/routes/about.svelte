1e477fdeb47e92e671cb0ae16accb2b42d61ffb0
Jolterix20 <jayeshvbane@gmail.com>
2022-01-18 12:19:53 +0530

    Initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+		neque id, accusamus natus, repudiandae sapiente perspiciatis ex numquam
+		incidunt magnam ipsa soluta facere voluptas corporis blanditiis
+		quibusdam qui maxime? Fugiat ratione quis saepe commodi ex animi sit at
+		eaque suscipit ducimus expedita accusamus quibusdam, iusto a voluptatum
+		sunt dolorem illo adipisci aperiam minima voluptatem. Explicabo modi
+		autem delectus porro voluptatum?
+	</p>
+	<a href="/guides">View Guides</a>
+	<a href="/">Home</a>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<div class="index">
+	<h2>Welcome</h2>
+	<p>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga ratione
+		obcaecati officiis similique vero dolor reprehenderit culpa adipisci
+		provident voluptates saepe unde exercitationem suscipit doloribus, quo
+		nisi temporibus consequatur vel magni quaerat rerum, odit sunt
+		aspernatur aut. Hic fugit velit repudiandae voluptatibus accusamus, non
+		ipsam, perspiciatis quisquam explicabo, architecto delectus!
+	</p>
+
+	<a href="/guides">View Guides</a>
+	<a href="/about">About Us</a>
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
